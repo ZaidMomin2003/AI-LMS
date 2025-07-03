fb9bab1479d3f3189e5962510c0d630a26781564
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-07-03 14:02:33 +0000

    In hero section, add icons hovering effect which are relevant to the app

--- src/components/landing/FloatingIcons.tsx
+++ src/components/landing/FloatingIcons.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BookOpen, BrainCircuit, Lightbulb, Atom, Dna, Microscope, GraduationCap, TestTube2 } from 'lucide-react';
+import { BookOpen, BrainCircuit, Lightbulb, Atom, GraduationCap } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const FloatingIcon = ({ icon: Icon, className, style }: { icon: React.ElementType, className?: string, style?: React.CSSProperties }) => {
@@ -22,9 +22,6 @@ export function FloatingIcons() {
             <FloatingIcon icon={BookOpen} className="w-14 h-14 bottom-[15%] left-[25%]" style={{ animationDelay: '1s', animationDuration: '10s' }} />
             <FloatingIcon icon={GraduationCap} className="w-20 h-20 bottom-[25%] right-[15%]" style={{ animationDelay: '2.5s', animationDuration: '8s' }} />
             <FloatingIcon icon={Atom} className="w-10 h-10 top-[60%] left-[8%]" style={{ animationDelay: '0.5s', animationDuration: '11s' }} />
-            <FloatingIcon icon={Microscope} className="w-12 h-12 top-[70%] right-[22%]" style={{ animationDelay: '3s', animationDuration: '6s' }} />
-            <FloatingIcon icon={Dna} className="w-8 h-8 top-[40%] left-[30%]" style={{ animationDelay: '2s', animationDuration: '12s' }} />
-            <FloatingIcon icon={TestTube2} className="w-10 h-10 bottom-[5%] right-[40%]" style={{ animationDelay: '3.5s', animationDuration: '7.5s' }} />
         </div>
     );
 }
