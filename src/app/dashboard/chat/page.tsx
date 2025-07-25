24750990c273dfe113475ee886a5e31fb9553d43
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-07-25 01:11:59 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ChatSidebar>
    
    wit

--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -1,16 +1,12 @@
 
 import { AppLayout } from '@/components/AppLayout';
 import { ChatMain } from '@/components/chat/ChatMain';
-import { ChatSidebar } from '@/components/chat/ChatSidebar';
 
 export default function ChatPage() {
     return (
         <AppLayout>
             <div className="flex-1 p-4 h-full">
-                <div className="grid grid-cols-1 lg:grid-cols-[300px_1fr] h-full gap-4">
-                    <ChatSidebar />
-                    <ChatMain />
-                </div>
+                <ChatMain />
             </div>
         </AppLayout>
     )
