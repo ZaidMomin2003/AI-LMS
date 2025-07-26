3120af16f4ed153f9f2f41f591cfb4a2da5fc3a2
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-07-26 17:31:56 +0000

    its chaging for a while and then going back to default favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.jpg" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
