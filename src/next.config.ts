871d522206ba669f80867db2c840a0ec5321388a
Arshadbasha Momin <arshadbashamomin@gmail.com>
2025-12-27 13:59:22 +0000

    Make the following changes:
    Find this element:
    
      <Primitive.li role="s

--- src/next.config.ts
+++ src/next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
   },
   experimental: {
     allowedDevOrigins: [
